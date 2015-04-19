62d4bab2da35109d99f42c83f61036db6f70788f
wolframowy <wolframowy@o2.pl>
2015-04-20 00:16:04 +0200

    Uploading base project

---
+++ Assets/Test_folder/PlayerCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public GameObject player;
+	public float torque;
+
+
+	private Vector3 offset;
+	private Quaternion lastRotation;
+	private float dest;
+	private int k;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		lastRotation = player.transform.rotation;
+		k = 0;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		dest = Input.GetAxis("Vertical");
+		transform.position = player.transform.position + offset;
+		if(dest > 0) k = 1;
+		else if(dest < 0) k = -1;
+			transform.RotateAround (player.transform.position,
+									transform.up,
+									k*Quaternion.Angle(player.transform.localRotation, lastRotation));
+		offset = transform.position - player.transform.position;
+		lastRotation = player.transform.rotation;
+	}
+}
---
+++ Assets/Test_folder/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private float moveVertical;
+	private float strafe;
+	private float turn;
+
+	public float speed;
+	public float torque;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		moveVertical = Input.GetAxis("Jump");
+		strafe = Input.GetAxis("Horizontal");
+		turn = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (strafe, moveVertical, 0);
+		rb.AddForce(movement * speed);
+		rb.AddTorque(transform.up * torque * turn);
+		
+	}
+}
