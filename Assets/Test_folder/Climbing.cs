85be195597598436a1c1759e7a2040a5a90cc056
Chmielok <chmielok@gmail.com>
2015-05-22 16:05:25 +0200

    Real start.

---
+++ Assets/Test_folder/Climbing.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Climbing : MonoBehaviour {
+
+	private enum Location {LEFT, MIDDLE, RIGHT};
+	private Location vine;
+	private float nextVineChange = 0.0f;
+
+	public float speed;
+	public float vineChangeCooldown = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		vine = Location.MIDDLE;
+	}
+
+	void FixedUpdate () {
+		float moveVer = Input.GetAxis ("Vertical");
+		float moveHor = Input.GetAxis ("Horizontal");
+		float leftOrRight = 0;
+		if (moveVer < 0 && transform.position.y < 0.7f)
+			moveVer = 0;
+		if (moveHor > 0 && vine != Location.RIGHT && Time.time > nextVineChange) {
+			nextVineChange = Time.time + vineChangeCooldown;
+			leftOrRight = 1;
+			vine += 1;
+		} else if (moveHor < 0 && vine != Location.LEFT && Time.time > nextVineChange) {
+			nextVineChange = Time.time + vineChangeCooldown;
+			leftOrRight = -1;
+			vine -= 1;
+		}
+
+		Vector3 movement = new Vector3 (leftOrRight, speed * moveVer, 0);
+		transform.Translate (movement);
+	}
+}
