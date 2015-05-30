71400af6b38dac4036650301fd65372db42ddd8b
wolframowy <wolframowy@o2.pl>
2015-05-30 20:28:03 +0200

    Panda is climbing with one imaginary hand. Grab and drag with mouse while holding LMB, or hold in place to prevent panda from falling.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour {
 	private float mouseSpeedX;
 	private float mouseSpeedY;
 	private Event mouseEvent;
+	private Vector3 mousePosition;
+	private Vector3 oldMousePosition;
+	private Vector3 lockPosition;
 
 	public float speed;
 	public Text mouseDelta;
@@ -17,6 +20,7 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		oldMousePosition = mousePosition = Vector3.zero;
 		mouseSpeedY = 0f;
 		pandaYSpeed.text = "Panda's Y velocity: " + rb.velocity.y.ToString();
 	}
@@ -32,9 +36,20 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		moveVertical = mouseSpeedY * speed;
-		rb.AddForce(new Vector3(0, moveVertical, 0), ForceMode.VelocityChange);
+		mousePosition = Input.mousePosition;
+		rb.useGravity = true;
+		if(Input.GetKey(KeyCode.Mouse0)){
+			moveVertical = (oldMousePosition.y - mousePosition.y) * speed;
+			if(moveVertical == 0) {
+				rb.useGravity = false;
+				rb.velocity = Vector3.zero;
+			}
+			else {
+				rb.velocity = new Vector3(0, moveVertical, 0);
+			}
+		}
 		pandaYSpeed.text = "Panda's Y velocity: " + rb.velocity.y.ToString();
+		oldMousePosition = mousePosition;
 	}
 
 	void SetMouseDelta () {
