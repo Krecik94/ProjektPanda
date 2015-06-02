e59f61f1b397ddb452289880b026b401608e7325
Chmielok <chmielok@gmail.com>
2015-06-03 00:16:30 +0200

    Added menu and pickup (apple) generator.

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Pause : MonoBehaviour {
+
+	private bool pauseGame=false;
+	private bool showUI=true;
+	void Start(){
+		(GameObject.Find ("GuiTexture").GetComponent<GUITexture> ()).enabled = false;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown("p")){
+
+		pauseGame = !pauseGame;
+
+		if(pauseGame == true)
+		{
+			Time.timeScale=0;
+			pauseGame=true;
+
+			GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
+			showUI=true;
+		}
+
+		if(pauseGame == false)
+		{
+			Time.timeScale=1;
+			pauseGame=false;
+	
+			GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
+			showUI=false;
+		}
+		if (showUI == true){
+
+			(GameObject.Find("GuiTexture").GetComponent<GUITexture>()).enabled=true;
+		}
+
+		if (showUI == false){
+				
+			(GameObject.Find("GuiTexture").GetComponent<GUITexture>()).enabled=false;
+		}
+
+			if(Input.GetKeyDown("space")){
+				Application.Quit();
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/PickupGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupGenerator : MonoBehaviour {
+	private float nextAppleSpawn = 15.95f;
+	private float nextSpawnTrigger = 10f;
+	
+	public GameObject Apple;
+	public float appleIntensity;
+
+	void FixedUpdate () {
+		if (transform.position.y > nextSpawnTrigger) {
+			nextSpawnTrigger += appleIntensity;
+			float number = Random.value;
+			
+			if (number>0.7f)
+				generateApple();
+			nextAppleSpawn += appleIntensity;
+		}
+	}
+
+	void generateApple() {
+		float number = Random.value;
+		float position;
+		
+		if (number <= 0.333f)
+			position = -1.5f;
+		else if (number <= 0.666)
+			position = 0f;
+		else
+			position = 1.5f;
+		
+		GameObject clone = (GameObject)Instantiate (Apple, new Vector3(position, nextAppleSpawn, 0f), Apple.transform.rotation);
+		clone.tag="Apple";
+	}
+}
