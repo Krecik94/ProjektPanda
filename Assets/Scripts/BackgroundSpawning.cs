2c992df64685ac59e21be8e6fc2ba3209c8abeed
Chmielok <chmielok@gmail.com>
2015-06-02 21:48:35 +0200

    Preparing for vine generation

---
+++ Assets/Scripts/BackgroundSpawning.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Climbing : MonoBehaviour {
+	private float nextTreeSpawn = 5.0f;
+
+	public GameObject Background;
+
+	void FixedUpdate () {
+		if (transform.position.y > nextTreeSpawn) { //spawnowanie tekstury drzewa (warto by bylo jeszcze usuwac tekstury, ktore juz dawno przestal widziec)
+			nextTreeSpawn += 10;
+			Instantiate (Background, new Vector3(0.0f, nextTreeSpawn, 0.45f), Background.transform.rotation);
+		}
+	}
+}
