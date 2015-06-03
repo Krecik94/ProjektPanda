6e2a14dd04d1d38919fd281762d2ab7436a576bc
Krecik <Marcin.Jarzebski94@gmail.com>
2015-06-03 17:26:40 +0200

    added pickups

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ROtator : MonoBehaviour {
+
+
+	private Quaternion rotation1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		transform.rotation= Quaternion.Euler (transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y+2,transform.rotation.eulerAngles.z);
+
+	}
+
+}
